f85eed605299caa3342150ea7b34c74b0e004d46
MateusDiasSilva <mateusdiassilva02@gmail.com>
2022-11-18 15:05:02 -0300

    Atualizaçao 4

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import model.entities.Department;
 import model.entities.Seller;
+import model.entities.dao.DaoFactory;
+import model.entities.dao.SellerDao;
 
 import java.util.Date;
 
@@ -12,6 +14,8 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
